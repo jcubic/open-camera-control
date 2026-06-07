d593a258b347f7fd1d76990fff31863c3b96f77a
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-06-07 13:45:02 +0200

    fix KeyboardInterrupt exception on CTRL+C

--- src/opencameracontrol/main.py
+++ src/opencameracontrol/main.py
@@ -1,3 +1,5 @@
+import signal
+
 import gi
 
 gi.require_version("Gtk", "3.0")
@@ -8,6 +10,7 @@ from opencameracontrol.gui import CameraControlWindow
 
 
 def main():
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     config = load_config()
     win = CameraControlWindow(config)
     win.show_all()
